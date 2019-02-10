7c78d85347f7db16949999b287114ef7280a4a72
Asif Mallik <asif.mallik1@gmail.com>
2019-02-10 15:24:53 -0500

    Add development (truffle develop) to networks in truffle.js

--- smart-contract/truffle.js
+++ smart-contract/truffle.js
@@ -3,7 +3,12 @@ const config = require("./config.json");
 
 module.exports = {
 	networks: {
-		ropsten: {
+        development: {
+            port: 9545,
+            host: "127.0.0.1",
+            network_id: "*",
+        },
+        ropsten: {
 			provider: () => new HDWalletProvider(config.wallet_private_key, "https://rinkeby.infura.io/v3/" + config.infura_api_key),
 			network_id: 4,
 			gas: 3000000,
