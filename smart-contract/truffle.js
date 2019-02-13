dae55d6d33c6c3ca75ad80baedc21ee19cc4ddcf
Asif Mallik <asif.mallik1@gmail.com>
2019-02-13 16:13:27 -0500

    change configuration to include ethereum network data

--- smart-contract/truffle.js
+++ smart-contract/truffle.js
@@ -1,18 +1,14 @@
 const HDWalletProvider = require("truffle-hdwallet-provider");
 const config = require("../config.json");
+console.log(JSON.stringify(config))
+Object.keys(config).forEach((key) => {
+    let network = config[key];
+    if (network.provider_data) {
+        network.provider = () => new HDWalletProvider(network.provider_data.wallet_private_key, network.provider_data.http_endpoint);
+    }
+});
 
 module.exports = {
-	networks: {
-        development: {
-            port: 9545,
-            host: "127.0.0.1",
-            network_id: "*",
-        },
-        ropsten: {
-			provider: () => new HDWalletProvider(config.wallet_private_key, "https://rinkeby.infura.io/v3/" + config.infura_api_key),
-			network_id: 4,
-			gas: 3000000,
-			gasPrice: 10000000000
-		}
-	}
+    networks: config
 };
+
